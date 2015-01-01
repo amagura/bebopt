0edf7533b9fb3e7f6798258a5e8bc96a31b9cb3f
amagura <sickhadas.code@gmail.com>
2015-01-01 13:22:34 -0700

    working on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,28 @@ var gulp    = require('gulp')
   , gutil   = require('gulp-util')
   , coffee  = require('gulp-coffee')
   , clint   = require('gulp-coffeelint')
+  , gpp     = require('gulp-preprocess')
   ;
+
+var srcdir = './src/';
 
 gulp.task('default', [ 'coffeelint', 'pp' ], function(){});
 gulp.task('build', [ 'default', 'coffee', 'package' ], function(){});
+
 gulp.task('coffee', function() {
-  gulp.src('*.coffee')
-    .pipe(coffee({ bare: true }).on('error', gutil.log));
+  gulp.src(srcdir + '/*.coffee')
+    .pipe(coffee({ bare: true }).on('error', gutil.log))
+    .pipe(gulp.dest(srcdir + '/'));
 });
+
 gulp.task('coffeelint', function() {
-  gulp.src('./lib/*.coffee')
+  gulp.src(srcdir + '/*.coffee')
     .pipe(clint())
-    .pipe(clint.reporter())
+    .pipe(clint.reporter());
 });
+
+gulp.task('pp', function() {
+  gulp.src('./src/*.coffee')
+    .pipe(gpp({ context: { DEBUG: false }}));
+});
+
