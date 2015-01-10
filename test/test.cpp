eff22a8fd7cd9d9d201a7b15bbf1fae2ebf49a25
amagura <sickhadas.code@gmail.com>
2015-01-09 21:33:59 -0700

    tests

---
+++ test/test.cpp
@@ -0,0 +1,16 @@
+//SKIP()\\
+# file: f
+
+#define both(...) \
+  ((r=0)) \
+  coffee -- ../example.coffe __VA_ARGS__ \
+  ((r=$?)) \
+  node -- ../example.js __VA_ARGS__ \
+  ((++r))
+
+test_Invalids () {
+  both "--help-x"
+  assertEquals "bad long options cause fail?" 2 "$r"
+  both "-z"
+  assertEquals "bad short options cause fail?" 2 "$r"
+}
